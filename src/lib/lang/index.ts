aa3afb6f7b4bfcd35043dd9bdf892bf6908e2059
Madis Otenurm <Madis0@users.noreply.github.com>
2024-02-13 15:04:26 +0100

    Update index.ts

--- src/lib/lang/index.ts
+++ src/lib/lang/index.ts
@@ -1,7 +1,13 @@
 import en from "./en.json";
 import et from "./et.json";
+import ko from "./ko.json";
+import ms from "./ms.json";
+import zlm from "./zlm.json";
 
 export const langs: Record<string, Record<string, string>> = {
     en,
-    et
+    et,
+    ko,
+    ms,
+    zlm
 };
