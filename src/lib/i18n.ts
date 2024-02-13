925549b9e17ed6982c7cbdfee41f5c0db4ef8c95
Skye <me@skye.vg>
2024-02-13 23:36:18 +0900

    fix(i18n): tauri's locale data seems to be wrong

--- src/lib/i18n.ts
+++ src/lib/i18n.ts
@@ -1,12 +1,7 @@
-import { locale as getLocale } from '@tauri-apps/api/os';
 import { langs } from './lang';
 
-let locale = navigator.language.split('-')[0];
+const locale = navigator.language.split('-')[0];
 const defaultLocale = 'en';
-
-getLocale().then((systemLocale) => {
-	if (systemLocale) locale = systemLocale.split('-')[0];
-});
 
 export function trans(id: string, data?: Record<string, string | number | undefined>) {
 	if (locale && langs[locale] && langs[locale][id]) {
