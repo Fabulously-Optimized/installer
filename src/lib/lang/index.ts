605117f028efdb1ee652dc6a782c6a408363e834
Madis Otenurm <Madis0@users.noreply.github.com>
2025-07-20 11:07:03 +0200

    Add new langs to index.ts

--- src/lib/lang/index.ts
+++ src/lib/lang/index.ts
@@ -1,4 +1,6 @@
+import be from './be.json';
 import en from './en.json';
+import es_es from './es-es.json';
 import et from './et.json';
 import fr from './fr.json';
 import he from './he.json';
@@ -13,20 +15,23 @@ import qep from './qep.json';
 import ro from './ro.json';
 import ru from './ru.json';
 import tr from './tr.json';
+import uk from './uk.json';
 import vi from './vi.json';
 import zh_cn from './zh-cn.json';
 import zh_tw from './zh-tw.json';
 import zh from './zh.json';
 
 export const langs: Record<string, [Record<string, string>, boolean]> = {
+	be: [be, false],
 	en: [en, false],
+	'es-es': [es_es, false],
 	et: [et, false],
 	fr: [fr, false],
 	he: [he, true],
 	it: [it, false],
 	ko: [ko, false],
-	'ms-arab': [ms_arab, true],
 	ms: [ms, false],
+	'ms-arab': [ms_arab, true],
 	pl: [pl, false],
 	'pt-br': [pt_br, false],
 	'pt-pt': [pt_pt, false],
@@ -34,6 +39,7 @@ export const langs: Record<string, [Record<string, string>, boolean]> = {
 	ro: [ro, false],
 	ru: [ru, false],
 	tr: [tr, false],
+	uk: [uk, false],
 	vi: [vi, false],
 	'zh-cn': [zh_cn, false],
 	'zh-tw': [zh_tw, false],
